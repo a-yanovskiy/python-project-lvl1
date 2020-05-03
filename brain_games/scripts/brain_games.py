8a7fd7876f25bc240a88a6d2506325d42ded720f
artem yanovskiy <a.e.yanovskiy@gmail.com>
2020-05-03 13:37:54 +0300

    linter fixes

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,14 @@
+"""
+Script brain_games.
+
+Simple console games. Project #1
+"""
+
 from brain_games.cli import welcome_user
 
 
 def main():
+    """Return greeting to new user."""
     return 'Welcome to the Brain Games!\n'
 
 
