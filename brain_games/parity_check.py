a17c2ce1f70c0731f4a9611ed1277004ac26de46
artem yanovskiy <a.e.yanovskiy@gmail.com>
2020-05-10 15:51:34 +0300

    Game fix

--- brain_games/parity_check.py
+++ brain_games/parity_check.py
@@ -9,30 +9,34 @@ from random import randint
 
 from prompt import string
 
-
-print('Welcome to the Brain Games!')
-print('Answer "yes" if number even otherwise answer "no".\n')
-
-
 from brain_games.cli import welcome_user
+
+print('Welcome to the Brain Games!')                            # noqa: WPS421
+print('Answer "yes" if number even otherwise answer "no".\n')   # noqa: WPS421
 
 name = welcome_user()
 
-print('Hello, {0}!\n'.format(name))
+print('Hello, {0}!\n'.format(name))                             # noqa: WPS421
 
-def parity(digit):    # проверяет рандомное число на четность
+
+def parity(digit):                                              # проверяет рандомное число на четность
     """Parity of random digit.
 
+    Parameters:
+        digit: argument, which must be check for parity.
+
     Returns:
-        Right answer.
+        'yes' or 'no': string.
     """
+    answer = ''
     if digit % 2 == 0:
-        return 'yes'
+        answer = 'yes'
     else:
-        return 'no'
+        answer = 'no'
+    return answer
 
 
-def input_answer():    # значение вводит пользователь
+def input_answer():                                             # значение вводит пользователь
     """
     User answer.
 
@@ -40,26 +44,27 @@ def input_answer():    # значение вводит пользователь
         User input.
     """
     user_answer = string('Your answer: ')
-    return user_answer
+    return user_answer                                          # noqa: WPS331
 
 
-def parity_check():    # сравнение результатов
-    """
-    Parity check game.
-    """
+def parity_check():                                             # сравнение результатов
+    # Parity check game.
+    
     count = 0
     right_ans = ''
     user_ans = ''
     global name
-    while count != 3:
-        random_digit = randint(0,100)
+    while count < 3:
+        random_digit = randint(0, 100)
         print(random_digit)
         right_ans = parity(random_digit)
         user_ans = input_answer()
-        if right_ans == user_ans:
-            print('Correct!')
-            count += 1
-        else:
+        if right_ans != user_ans:
             print("'{0}' is wrong answer ;(. Correct answer was '{1}'.\nLet's try again, {2}!".format(user_ans, right_ans, name))
             break
+        else:
+            print('Correct!')
+            count += 1
+            if count < 3:
+                continue
         print('Congratulations, {0}!'.format(name))
