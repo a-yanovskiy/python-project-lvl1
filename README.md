f6b0d163d2274d3eb9885f3e0181d2b6d1dd433b
artem yanovskiy <a.e.yanovskiy@gmail.com>
2020-05-19 15:05:05 +0300

    Add GCD-game

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 <a href="https://codeclimate.com/github/codeclimate/codeclimate/test_coverage"><img src="https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/test_coverage" /></a>
 
 ## Brain-games
-[![asciicast](https://asciinema.org/a/Og7KPonjEfseGyXpZsiVcPTXu.svg)](https://asciinema.org/a/Og7KPonjEfseGyXpZsiVcPTXu)
+<a href="https://asciinema.org/a/tjPWbnrGalgbwdQAsvjUgUPOx" target="_blank"><img src="https://asciinema.org/a/tjPWbnrGalgbwdQAsvjUgUPOx.svg" /></a>
---
+++ brain_games/games/gcd.py
@@ -0,0 +1,58 @@
+"""
+Игра "НОД".
+
+Суть игры в следующем:
+Пользователю показывается два случайных числа.
+Пользователь должен вычислить и ввести наибольший общий делитель этих чисел.
+"""
+
+from random import randint
+
+game_rules = 'Find the greatest common divisor of given numbers.\n'
+
+
+def random_digit():
+    """Random digit.
+
+    Returns:
+        random digit (1,100).
+    """
+    return randint(1, 100)  # noqa: S311
+
+
+def question():
+    """Question.
+
+    Returns:
+        Question: string.
+    """
+    return ('{0} {1}'.format(random_digit(), random_digit()))
+
+
+def answer(from_question):  # noqa: WPS210
+    """Right answer.
+
+    Parameters:
+        from_question: arg from question()
+
+    Returns:
+        Right answer.
+    """
+    digits_str = from_question.split()  # сплитим str значения из question()
+
+    # загоняем их в переменные:
+    first_random_digit = int(digits_str[0])
+    second_random_digit = int(digits_str[1])
+
+    # решение
+    if first_random_digit == second_random_digit:
+        ans = first_random_digit
+    else:
+        min_digit = min(first_random_digit, second_random_digit)
+        max_digit = max(first_random_digit, second_random_digit)
+        ans = min_digit
+        while max_digit % ans != 0:
+            ans -= 1
+            while min_digit % ans != 0:
+                ans -= 1
+    return str(ans)
